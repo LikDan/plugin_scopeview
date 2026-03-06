be08e4c42ffb6593e262178bd364535d77d95cb2
GitHub Action <action@github.com>
2026-03-06 22:12:25 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/likdan/pluginscopeview/view/nodes/MultiScopeRootNode.kt
@@ -0,0 +1,20 @@
+package com.github.likdan.pluginscopeview.view.nodes
+
+import com.intellij.ide.projectView.PresentationData
+import com.intellij.ide.projectView.ViewSettings
+import com.intellij.ide.util.treeView.AbstractTreeNode
+import com.intellij.openapi.project.Project
+
+class MultiScopeRootNode(
+    project: Project,
+    private val settings: ViewSettings,
+    private val scopeNames: List<String>,
+) : AbstractTreeNode<String>(project, "root") {
+
+    override fun update(presentation: PresentationData) {
+        presentation.presentableText = "root"
+    }
+
+    override fun getChildren(): Collection<AbstractTreeNode<*>> =
+        scopeNames.map { ScopeNode(project, settings, it) }
+}
--- src/main/kotlin/org/jetbrains/plugins/template/view/settings/ScopeGroup.kt
+++
@@ -1,6 +0,0 @@
-package org.jetbrains.plugins.template.view.settings
-
-data class ScopeGroup(
-    var name: String = "",
-    var scopes: MutableList<String> = mutableListOf()
-)
